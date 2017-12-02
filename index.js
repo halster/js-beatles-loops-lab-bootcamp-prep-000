36a7cada0faef8442b45f1378385d500de673e07
Hallee Palmer <pi.is.good.3.141@gmail.com>
2017-12-02 03:52:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function iLoveTheBeatles(number){
 var i=0
 var array=[]
   do {
-    array[i]=["I love the Beatles!"]
+    array[i]="I love the Beatles!"
     i++
   }
   while (i<number&& number<15)
